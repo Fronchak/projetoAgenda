3769af7655b6f86771601c1ab85a73029dd33d6c
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-08-21 16:00:14 -0300

    Concluído validação ao criar nova conta

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcryptjs = require("bcryptjs");
 
 const LoginSchema = new mongoose.Schema({
     email: {type: String, required: true},
@@ -18,7 +19,16 @@ class Login {
     async register() {
         this.valida();
         if(this.isThereAnyErros()) return;
+
+        await this.userExists();
+
+        if(this.isThereAnyErros()) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
         try {
+
             this.user = await LoginModel.create(this.body);
         }
         catch(e) {
@@ -44,6 +54,11 @@ class Login {
             email: this.body.email,
             password: this.body.senha
         };
+    }
+
+    async userExists() {
+        const user = await LoginModel.findOne({email: this.body.email});
+        if(user) this.erros.push("Já existe um cadastro com esse e-mail");
     }
 
     isThereAnyErros = () => this.erros.length > 0;
